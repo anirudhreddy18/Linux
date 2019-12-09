0ee56ca58f3197e18725e51add4439381a9abbf1
Anirudh Vemula <anirudh.anirudh15@gmail.com>
2019-12-08 18:43:08 -0600

    added shell scripyt

---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo 'Hello'
+
+# Variables
+temp='Script'
+
+echo $temp
+
+#Display variable assigned
+echo "This is a shell $temp"
+
+#Display USer id
+echo "UID is $UID"
+
+#Assign to a variable and display
+UNAME=$(id -un)
+echo "USERNAME IS $UNAME"
+
+#Assign to a variable and print exact USERNAME=vagrant
+echo USERNAME=$(id -un)
+
+#if/else statement
+if [[ $UID -eq 0 ]]
+then
+  echo "You are root."
+else
+  echo "You are non root."
+fi  
+
+echo "Current UserID is $UID"
+
+if [[ UID -ne 1000 ]]
+then
+  echo "Current UID is $UID"
+  #exit
+fi 
+
+echo "Current USER is $USER"
+
+#if/else statement
+if [[ $USER = vagrant ]]
+then
+  echo "You are right user."
+else
+  echo "You are not right user."
+fi  
+
+USERNAME="wtps"
+useradd -c "Anirudh Vemula" -m $USERNAME
+
+echo anirudh | passwd --stdin $USERNAME
+
+passwd -e $USERNAME
